f2102b81342ee05104b257954b861cc84894d427
Никита Россиин <n.rossiin98@yandex.ru>
2023-04-10 01:02:10 +0300

    sp

--- NDFL_project/get_report/forms.py
+++ NDFL_project/get_report/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .models import Report
 
 class ReportForm(forms.Form):
     excel_file = forms.FileField(label='Загрузите файл Excel')
--- NDFL_project/get_report/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Report(models.Model):
-    report = models.FileField(upload_to='')
